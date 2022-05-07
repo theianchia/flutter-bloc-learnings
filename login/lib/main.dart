a1bd3c67e5b40eb54ad8db23a1e832dc281b821a
theianchia <theianchia@users.noreply.github.com>
2022-05-07 18:02:48 +0800

    Added login tutorial

---
+++ login/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/widgets.dart';
+import 'package:login/app.dart';
+import 'package:user_repository/user_repository.dart';
+
+void main() {
+  runApp(App(
+    authenticationRepository: AuthenticationRepository(),
+    userRepository: UserRepository(),
+  ));
+}
